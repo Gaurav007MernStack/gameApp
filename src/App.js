3b6b8035288a90ea47ee58015ad9143d54f0f9de
Mohd Abdulla <mdabdulla1507@gmail.com>
2021-12-20 19:32:28 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Board from "./Board";
 import Square from "./Square";
 import { useState, useEffect } from "react";
-
+import { useStopwatch } from "react-use-stopwatch";
 const defaultSquares = () => new Array(9).fill(null);
 
 const lines = [
@@ -17,56 +17,77 @@ const lines = [
 ];
 
 function App() {
-  const [squares, setSquares] = useState(defaultSquares());
-  const [winner, setWinner] = useState(null);
-  const [playerTimeLimit, setPlayerTimeLimit] = useState(true);
+  const [{ time }, start, stop, reset] = useStopwatch();
 
   useEffect(() => {
-    console.log("playerLimit", playerTimeLimit);
-  }, [playerTimeLimit]);
-
-  useEffect(() => {
-    setTimeout(setTime, 6000);
-    function setTime() {
-      setPlayerTimeLimit(false);
-    }
-    let isComputerTurn =
-      squares.filter((square) => square !== null).length % 2 === 1
-    if(!isComputerTurn){
-      if(!playerTimeLimit){
-        isComputerTurn = true
-        setPlayerTimeLimit(true)
+    const ab = squares.filter((item) => item != null);
+    // console.log("time: ", time);
+    if (time === 0 && !winner) {
+      aiTurn();
+    } else {
+      if (ab.length === 0 && time > 6000) {
+        reset();
+        alert("Please click Start again");
       }
     }
-    const linesThatAre = (a, b, c) => {
-      return lines.filter((squareIndexes) => {
-        const squareValues = squareIndexes.map((index) => squares[index]);
-        return (
-          JSON.stringify([a, b, c].sort()) ===
-          JSON.stringify(squareValues.sort())
-        );
-      });
-    };
-    const emptyIndexes = squares
-      .map((square, index) => (square === null ? index : null))
-      .filter((val) => val !== null);
-    const playerWon = linesThatAre("x", "x", "x").length > 0;
-    const computerWon = linesThatAre("o", "o", "o").length > 0;
-    if (playerWon) {
-      setWinner("x");
-    }
-    if (computerWon) {
-      setWinner("o");
-    }
-    const putComputerAt = (index) => {
-      let newSquares = squares;
-      newSquares[index] = "o";
-      setSquares([...newSquares]);
-    };
-    if (isComputerTurn) {
-      setTimeout(myFunction, 1000);
+  }, [time]);
+  const [squares, setSquares] = useState(defaultSquares());
 
-      function myFunction() {
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    console.log('winner: ', winner);
+    if (winner) {
+      
+      reset();
+
+      setSquares(defaultSquares);
+      alert("Start again");
+    }
+  }, [winner]);
+  
+  const linesThatAre = (a, b, c) => {
+    return lines.filter((squareIndexes) => {
+      const squareValues = squareIndexes.map((index) => squares[index]);
+      return (
+        JSON.stringify([a, b, c].sort()) ===
+        JSON.stringify(squareValues.sort())
+      );
+    });
+  };
+  const aiTurn = () => {
+    const ab = squares.filter((item) => item != null);
+    // console.log("ab: ", ab);
+
+    if (ab.length !== 0 && time <= 0) {
+      console.log("inside");
+      let isComputerTurn = time <= 0;
+    
+
+      const emptyIndexes = squares
+        .map((square, index) => (square === null ? index : null))
+        .filter((val) => val !== null);
+      const playerWon = linesThatAre("x", "x", "x").length > 0;
+      const computerWon = linesThatAre("o", "o", "o").length > 0;
+      if (playerWon) {
+        setWinner("x");
+      }
+      if (computerWon) {
+        setWinner("o");
+      }
+      const putComputerAt = (index) => {
+        let newSquares = squares;
+        newSquares[index] = "o";
+
+        setSquares([...newSquares]);
+        start();
+        // if (time == 0) {
+        //   setTimeout(() => {
+        //     start();
+        //   }, 1000);
+        // }
+      };
+      if (isComputerTurn) {
         const winingLines = linesThatAre("o", "o", null);
         if (winingLines.length > 0) {
           const winIndex = winingLines[0].filter(
@@ -98,21 +119,40 @@ function App() {
         putComputerAt(randomIndex);
       }
     }
-  }, [squares, playerTimeLimit]);
+  };
 
   function handleSquareClick(index) {
-    const isPlayerTurn =
-      squares.filter((square) => square !== null).length % 2 === 0;
-    console.log("isPlayerTurn", isPlayerTurn);
+    const isPlayerTurn = time > 0;
+    // console.log('isPlayerTurn: ', isPlayerTurn);
     if (isPlayerTurn) {
       let newSquares = squares;
       newSquares[index] = "x";
+
       setSquares([...newSquares]);
+      if (time > 0) {
+        reset();
+      }
     }
   }
 
   return (
     <main>
+      <div>
+        // Stopwatch Outputs
+        <strong>{time}</strong>
+        {/* <strong>{format}</strong> */}
+        // Stopwatch Inputs
+        <button
+          onClick={() => {
+            start();
+            setWinner(null);
+          }}
+        >
+          Start
+        </button>
+        {/* <button onClick={() => stop()}>Stop</button> */}
+      </div>
+
       <Board>
         {squares.map((square, index) => (
           <Square
